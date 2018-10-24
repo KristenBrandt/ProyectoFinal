34e1a5ec50f4333333e32d7d410c0a911494c1fa
KristenBrandt <kabrandtf@gmail.com>
2018-10-23 18:34:36 -0600

    LogIn and SignIn

--- app/src/main/java/com/example/kristen/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/kristen/proyectofinal/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.kristen.proyectofinal;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.EditText;
 
+import java.io.IOException;
+
+//Botton para hacer log in
 public class MainActivity extends AppCompatActivity {
+    public void firstButton() {
+        Button login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(log);
+            }
+        });
+    }
+// Botton para hacer sign in
+    public void secondButton() {
+        Button sign = (Button) findViewById(R.id.signin_button);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(log);
+            }
+        });
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            firstButton();
+            secondButton();
+        }
 }
+
+
+
+
+
